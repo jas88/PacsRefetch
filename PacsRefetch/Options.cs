fdf3bc8e7bf988c12bb4e1fec93d2b4f2012888f
James A Sutherland <j@sutherland.pw>
2024-04-16 22:14:37 +0100

    Feature/net8cleanup (#61)
    * .Net 8, cleanup, .gitignore
    
    * Update PacsRefetch.csproj
    
    Fix conflicting AOT vs singlefile options
    
    * Update PacsRefetch.csproj
    
    Set TrimmerSingleWarn false for more diagnostics

--- PacsRefetch/Options.cs
+++ PacsRefetch/Options.cs
@@ -8,15 +8,17 @@ namespace PacsRefetch;
 [UsedImplicitly]
 internal class Options
 {
-    internal static CancellationTokenSource cts=new();
+    internal static readonly CancellationTokenSource TokenSource = new();
 
     public PacsParameters Pacs { get; private set; } = new();
+
     private static DateTime ParseDateTime(string s)
     {
         if (DateTime.TryParseExact(s, "yyyyMMddHH", CultureInfo.InvariantCulture, DateTimeStyles.None, out var r))
             return r;
         if (DateTime.TryParseExact(s, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out r))
             return r;
+
         throw new ArgumentException($"Unrecognised date/time string '{s}'");
     }
 
@@ -24,7 +26,8 @@ internal class Options
     public string? Patient { get; set; } = null;
 
     internal DicomDateRange DicomWindow = null!;
-    [Option('r',"range",Required = true,HelpText = "Date/time range to search for; 1s subtracted from upper limit for convenience")]
+
+    [Option('r', "range", Required = true, HelpText = "Date/time range to search for; 1s subtracted from upper limit for convenience")]
     public string Window
     {
         get => DicomWindow.ToString();
@@ -41,7 +44,7 @@ internal class Options
         get => Pacs.ToString();
         set => Pacs = PacsParameters.Load(value);
     }
-    
+
     [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
     public bool Verbose { get; set; }
 }
